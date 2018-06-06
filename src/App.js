5b04a3c8a4a3dd0f4c5fd434240de47f8ef05987
Oganesyan Artur <arturris@mail.ru>
2018-06-06 14:07:27 -0500

    Basic exercises with Binding

--- src/App.js
+++ src/App.js
@@ -8,11 +8,29 @@ class App extends Component {
       {name: 'Artur', age: 22},
       {name: 'Max', age: 32},
       {name: 'Oleg', age: 42}  
-    ]
+    ],
+    otherValue: 'String'
   }
 
-  switchNameHandler = () => {
-    console.log('Clicked')
+  switchNameHandler = (newName) => {
+    // console.log('Clicked')
+    this.setState({
+      persons:[
+        { name: newName, age: 22 },
+        { name: 'Artur', age: 32 },
+        { name: 'Oleg', age: 42 }
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 22 },
+        { name: event.target.value, age: 32 },
+        { name: 'Oleg', age: 142 }
+      ]
+    })
   }
 
   render() {
@@ -20,10 +38,19 @@ class App extends Component {
       <div className="App">
        <h1>Hi, I'm React App</h1>
        <p>This is Working</p>
-       <button onClick={this.switchNameHandler}>Switch Names</button>
-       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies is: Soccer</Person>
-       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={this.switchNameHandler.bind(this, 'Looser')}>Switch Names</button>   
+        {/* <button onClick={() => this.switchNameHandler('Hulya!')}>Switch Names</button> Alternative way to bind  */}
+       <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age}/>
+       <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age}
+        click={this.switchNameHandler.bind(this, 'Not A Looser!')}
+        changed={this.nameChangedHandler}>My Hobbies is: Soccer</Person>
+       <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age}/>
       </div>
     );
   }

5c6a5a53f539ae4fe1a42804fde26ef9f1da7c0e
Oganesyan Artur <arturris@mail.ru>
2018-06-10 16:10:56 -0500

    Change Stateless component to Statefull

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -21,7 +21,7 @@ const cockpit  =(props) => {
 
     return (
         <div className={classes.Cockpit}>
-            <h1>Hi, I'm React App</h1>
+            <h1>{props.appTitle}</h1>
             <p className={assignedClasses.join(' ')}>This is Working</p>
             <button
                 className={btnClass}
--- src/containers/App.js
+++ src/containers/App.js
@@ -69,6 +69,7 @@ class App extends Component {
     return (
       <div className={classes.App}>
       <Cockpit 
+      appTitle={this.props.title}
       showPerson={this.state.showPerson}
       persons={this.state.persons}
       clicked={this.togglePersonsHandler}/>
